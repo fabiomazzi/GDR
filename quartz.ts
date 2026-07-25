48f9242dd15777bfb3106bfe3234549223647641
fabiomazzi <149215827+fabiomazzi@users.noreply.github.com>
2026-07-26 00:31:41 +0200

    Update quartz.ts

--- quartz.ts
+++ quartz.ts
@@ -1,14 +1,5 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 import * as ExternalPlugin from "./.quartz/plugins"
-
-ExternalPlugin.FolderPage({
-  sort: (a, b) => {
-    const titleA = a.frontmatter?.title ?? ""
-    const titleB = b.frontmatter?.title ?? ""
-    return titleA.localeCompare(titleB)
-  },
-})
-
 const config = await loadQuartzConfig()
 export default config
 export const layout = await loadQuartzLayout()
