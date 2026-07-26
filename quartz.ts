47df7e41b5c08e99cb944d0ad2c61feae2df2659
fabiomazzi <149215827+fabiomazzi@users.noreply.github.com>
2026-07-27 00:01:06 +0200

    Update quartz.ts

--- quartz.ts
+++ quartz.ts
@@ -1,20 +1,25 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
 import * as ExternalPlugin from "./.quartz/plugins"
 
-const config = await loadQuartzConfig()
-
-const customFolderPage = ExternalPlugin.FolderPage({
-  showFolderCount: false,
-  sort: (a, b) => {
-    const t1 = a.frontmatter?.title?.toLowerCase() ?? ""
-    const t2 = b.frontmatter?.title?.toLowerCase() ?? ""
-    return t1.localeCompare(t2)
-  },
+// Nasconde "nascosto" dall'Explorer (deve stare prima di loadQuartzConfig)
+ExternalPlugin.Explorer({
+  filterFn: (node) => node.slug !== "nascosto",
 })
 
-config.plugins.pageTypes = config.plugins.pageTypes.map((p) =>
-  p.name === "FolderPage" ? customFolderPage : p
-)
+const config = await loadQuartzConfig()
+
+// --- FolderPage custom (disattivato, tenuto per riferimento) ---
+// const customFolderPage = ExternalPlugin.FolderPage({
+//   showFolderCount: false,
+//   sort: (a, b) => {
+//     const t1 = a.frontmatter?.title?.toLowerCase() ?? ""
+//     const t2 = b.frontmatter?.title?.toLowerCase() ?? ""
+//     return t1.localeCompare(t2)
+//   },
+// })
+// config.plugins.pageTypes = config.plugins.pageTypes.map((p) =>
+//   p.name === "FolderPage" ? customFolderPage : p
+// )
 
 export default config
 export const layout = await loadQuartzLayout()
